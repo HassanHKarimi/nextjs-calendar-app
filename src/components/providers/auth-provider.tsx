64479fec003922cccc9466b01141056e394b492c
HassanHKarimi <hassan.karimi@gmail.com>
2025-04-05 17:42:21 -0400

    Add auth provider component

---
+++ src/components/providers/auth-provider.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+
+export interface AuthProviderProps {
+  children: ReactNode
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
