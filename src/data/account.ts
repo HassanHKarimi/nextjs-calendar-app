15e907a1d3d7bf73df8c7196bf1b1dc3a55a1231
HassanHKarimi <hassan.karimi@gmail.com>
2025-04-06 09:14:48 -0400

    Add account data access file

--- src/data/account.ts
+++ src/data/account.ts
@@ -1,5 +1,10 @@
 import { db } from "@/lib/db";
 
+/**
+ * Get an account by user ID
+ * @param userId - The user ID to lookup
+ * @returns The account if found, null otherwise
+ */
 export const getAccountByUserId = async (userId: string) => {
   try {
     const account = await db.account.findFirst({
