11fb5b78d0fcc97432193d42b37f207142cb1b6a
HassanHKarimi <hassan.karimi@gmail.com>
2025-04-06 09:17:20 -0400

    Fix bcryptjs import in auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,10 @@
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import Google from "next-auth/providers/google"
+import bcrypt from "bcryptjs"
 
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/user"
-import bcrypt from "bcryptjs"
 
 export default {
   providers: [
